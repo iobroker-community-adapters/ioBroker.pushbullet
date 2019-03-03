f802867a52e5405e62c152d6e48c217b1e662c2b
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:44:45 +0100

    add adapter-core

--- pushbullet.js
+++ pushbullet.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // you have to require the utils module and call adapter function
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils = require('@iobroker/adapter-core'); // Get common adapter utils
 var adapter = utils.adapter('pushbullet');
 var PushBullet = require('pushbullet');
 
